f0b7d38ba7ce0ceaef1b7914afafc608dff7eeab
komma3459 <komma3459@gmail.com>
2023-04-25 09:34:57 +0900

    CBV 방식으로 views와 urls를 수정하기 - 2023 04 24

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('',views.index),
+    # CBV 기반 path
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
+
+    # FBV 기반인 views.index는 주석처리
+    # path('',views.index),
+    # path('<int:pk>/', views.single_post_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post
-        }
-    )
+# CBV (장고 제공 클래스 기반 views 제작)
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/index.html'
+    # 블로그 경로에 있는 index.html 이름을 post(모델명)_list로 고쳐버리기
+    # django.views.generic으로 부르는 ListView는
+    # 모델명_list.html을 기본 파일명으로 삼기 때문이다.
+    # ListView의 ordering = -pk는 포스트 목록 페이지의 포스트를 역순으로 정렬해줌
+
+class PostDetail(DetailView):
+    model = Post
+# 상세 페이지를 보여주기 위해 CBV의 DetailView를 불러옴
+# class 클래스명(DetailView) : ()안의 DeetailView를 () 밖의 클래스가 상속한다는 뜻
+# model = Post여야 하는 이유 : models.py에서 불러올 클래스가 Post이기 때문.
+# 이거 작성하고 urls 가서 path('<int:pk>/', views.PostDetail.as_view()) 작성해주기
+
+# FBV(함수 기반 views 제작)
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post
+#         }
+#     )
