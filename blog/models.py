85c623f22401208f850ca7eb58c4de3d9777c2d1
komma3459 <komma3459@gmail.com>
2023-05-05 15:28:26 +0900

    "2023 05 05 - 사용자(author) 필드를 추가하기 / author 변수와 f'[{self.pk}]{self.title}::{self.author} 를 이용해 /admin에 가서 사용자를 추가해보거나 기존 포스트 사용자를 변경해보기 "

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 import os.path
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -14,8 +14,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}::{self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
