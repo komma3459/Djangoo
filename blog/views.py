5bc0852623f44b42289aec8d045530a28ea652fa
komma3459 <komma3459@gmail.com>
2023-05-07 17:24:48 +0900

    "2023 05 07 - 사이트에서 미분류 카테고리를 클릭하면 미분류 페이지가 나오도록 합시다 (if문 안에 category 변수가 위치해야 예외사항이 생겨서 미분류 카테고리 페이지 경로 못찾는다는 에러가 안나요!)"

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,6 @@ from .models import Post, Category
 
 #FBV 함수 선언 제작 방식
 def category_page(request, slug):
-    category = Category.objects.get(slug=slug)
-
     if slug == 'no_category':
         category = '미분류'
         post_list = Post.objects.filter(category=None)
@@ -15,7 +13,7 @@ def category_page(request, slug):
         post_list = Post.objects.filter(category=category)
 
     return render(request, 'blog/post_list.html', {
-        'post_list': Post.objects.filter(category=category),
+        'post_list': post_list,
         'categories': Category.objects.all(),
         'no_category_post_count': Post.objects.filter(category=None).count(),
         'category': category,
