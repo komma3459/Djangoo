24e7ba0c578b61609baa39473a22ad9224c2ba5f
komma3459 <komma3459@gmail.com>
2023-05-10 17:42:07 +0900

    "django-crispy-forms 적용하기"

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
 
+    'crispy_forms',
+    'crispy_bootstrap4',
     'blog',
     'single_pages',
 ]
@@ -125,6 +127,8 @@ STATIC_URL = 'static/'
 
 MIDIA_URL = '/media/'
 MIDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
