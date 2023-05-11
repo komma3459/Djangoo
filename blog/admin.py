d19483d33b1f9f1e1b491133e79dfb70a93d5e75
komma3459 <komma3459@gmail.com>
2023-05-11 11:22:44 +0900

    "django에 Markdownx 불러온 후 post_d , post_list, admin에 markdown 기능 적용하고 실행해보기 "

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
 from .models import Post, Category, Tag
-# Register your models here.
-
-admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug' : ('name', )}
+    prepopulated_fields = {'slug': ('name', )}
 
 class TagAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug' : ('name', )}
+    prepopulated_fields = {'slug': ('name', )}
 
+admin.site.register(Post, MarkdownxModelAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
